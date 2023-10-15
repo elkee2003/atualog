62e9d5475625da43145f2ccd4313ee49998442e4
Elkanah <elkee2003@yahoo.com>
2023-10-15 09:07:08 +0100

    edit

--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -14,7 +14,7 @@ function Services() {
                   <p className='section-description card'>Our range of services are unending. We offer a platform for packages to be sent/delivered from one point to another. It does not matter the size of item/package that needs to be sent; it does not matter the time you want it sent, there will be users always ready to deliver your package.
                   </p>
                   <p className='section-description card'>
-                  Furthermore, we offer our users a chance to be couriers and make money for themselves at any given time. You are given to opportunity to have a full time job or part time job.
+                  Furthermore, we offer our users a chance to be couriers and make money for themselves at any given time. You are given an opportunity to have a full time job or part time job.
                   </p>
                 </div>
                 
